a3bfabb08511a7ffd8dc84d7e5fe53168cea710e
simranrawat <simranrawat2k@gmail.com>
2025-06-20 16:28:50 +0530

    login

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -1,40 +1,38 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    username:{
-        type: String,
-        required: true,
-        unique: true,
-
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    password:{
-        type:String,
-        required:true,
-
-    },
-    role:{
-        type:String,
-        default:"User",
-
-    },
-    subscription:[
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Course"
-        }
-    ],
-   
-
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  class: {
+    type: String, 
+    enum: ["VI", "VII", "VIII", "IX", "X", "XI", "XII"], 
+  },
+  status: {
+    type: String,
+    default: "active",
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false, 
+  },
 },
 {
-    timestamps:true,
-}
-)
-
-
+  timestamps: true,
+});
 export const UserSchema = mongoose.model("User",userSchema);
