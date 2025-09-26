a100f76833968407a29e2c5ec8e1f6da8291a5a1
Ruby123-creator <rubyp20001@gmail.com>
2025-09-26 16:14:30 +0530

    code-updated

--- server/controllers/courses.js
+++ server/controllers/courses.js
@@ -1,10 +1,5 @@
 import { TryCatch } from "../middleware/tryCatch.js"
-import { CourseSchema } from "../models/courseSchema.js"
-import { LectureSchema } from "../models/lectureSchema.js";
-import { UserSchema } from "../models/userSchema.js";
-import { instanace } from "../index.js";
-import crypto from 'crypto';
-import { PaymentSchema } from "../models/paymentSchema.js";
+
 import { Subject } from "../models/subjectSchema.js";
 export const getAllCourses = TryCatch(async (req,res)=>{
    const allCourses = await Subject.findById(req.params.userId);
