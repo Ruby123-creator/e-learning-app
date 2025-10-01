ac8ee54a335886ba0454bc391f93425502256247
simranrawat <simranrawat2k@gmail.com>
2025-10-01 15:14:07 +0530

    color change

--- frontend/src/Pages/Modules/Content.js
+++ frontend/src/Pages/Modules/Content.js
@@ -58,7 +58,7 @@ function Content() {
 
   return (
     <div className="chapter-container">
-      <h2 style={{ textAlign: "center" }}>{chapterName} Topics</h2>
+      <h2 style={{ textAlign: "center", color: "white" }}>{chapterName} Topics</h2>
 
       {/* Search Bar */}
       <div style={{ textAlign: "center", marginBottom: "20px" }}>
