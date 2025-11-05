3eb1702c733f34c32d73cfb8f6e719e2969b3deb
Ruby pal <rubyp20001@gmail.com>
2025-11-05 21:34:59 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import uploadRouter from './routes/upload.js'
 import subjectRouter from './routes/subject.js'
 import { connectDB } from './database/db.js';
 import cors from 'cors';
-import Razorpay from 'razorpay';
+
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpecs from './swagger.js';
 
@@ -17,16 +17,16 @@ const app = express();  //Used to create the server and manage routes.
 const port = process.env.PORT||8000
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
-console.log(process.env.razor_pay_key, process.env.razor_secret_key,"CHECK::")
- export const instanace = new Razorpay({
-  key_id: process.env.razor_pay_key,
-  key_secret: process.env.razor_secret_key
-})
+
 
 app.use(express.json());//Parses incoming JSON payloads.
 app.use(express.urlencoded({extended:true}));//Parses URL-encoded payloads. extended: true allows nested objects.
-app.use(cors()); //Middleware that enables CORS (Cross-Origin Resource Sharing). This allows the server to handle requests from different origins.
 
+app.use(cors({
+  origin: "https://topicwise.vercel.app", // replace with your frontend domain
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use('/uploads',express.static('uploads'));//Serves static files from the uploads directory when requested via /uploads.
 app.get('/',(req,res)=>{
     return res.send("my server is running");
